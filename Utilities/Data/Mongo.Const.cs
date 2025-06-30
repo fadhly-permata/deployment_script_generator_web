8d2fdf8ed8bfb055eb087f7620c00008fae90260
fadhly.permata <fadhly.permata@idxpartners.com>
2025-06-30 17:09:20 +0700

    Remove unused files

--- Utilities/Data/Mongo.Const.cs
+++
@@ -1,8 +0,0 @@
-namespace ScriptDeployerWeb.Utilities.Data;
-
-internal partial class Mongo
-{
-    private const string WF_CONFIG = "workflow.workflow_configurations";
-    private const string WF_PROCESS_LOG = "workflow.workflow_process_log";
-    private const string PUB_RULE_SET_HISTORY_PIC = "public.log_pic";
-}
